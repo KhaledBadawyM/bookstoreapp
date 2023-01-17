852495dfd0e1ea0538428a7fff9da9e662817917
KhaledBadawyM <khaledbadawi881@gmail.com>
2023-01-17 23:01:37 +0200

    update route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import userRouter from './userRoute'
+import productRoute from './productRoute'
 
 const router = Router()
 
 router.use('/', userRouter)
-router.use('/', userRouter)
+router.use('/product', productRoute)
 
 export default router
